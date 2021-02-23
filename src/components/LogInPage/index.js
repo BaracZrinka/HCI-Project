1abb93bce8e4c957ce055588655d01e1ce52b4b5
petraroso <proso00@fesb.hr>
2021-02-23 16:56:13 +0100

    login and signup functionality

--- src/components/LogInPage/index.js
+++ src/components/LogInPage/index.js
@@ -1,26 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {navigate} from 'gatsby'
 import styles from './style.module.css';
 
-const LogInPage = () => (
-    <div className={styles.form}>
-        
-        <div className={styles.login}>
-            <h2 className={styles.title}>Log in</h2>
-            <div className={styles.email}>
-                <label htmlFor="username">E-mail or username:</label>
-                <input name="username" type="text" />
-            </div>
+const users = [
+    {
+      username: "zrinka",
+      password: "123"
+    },
+    {
+      username: "petra",
+      password: "123"
+    },
+    ]
 
-            <div className={styles.password}>
-                <label htmlFor="password">Password:</label>
-                <input name="password" type="text" />
-            </div>
-            <div className={styles.buttondiv}>
-                <button className={styles.button}>Log in</button>
-            </div>
+const LogInPage = () => {
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+  
+    const submit = () => {
+      setError(false)
+      setLoading(true)
+      setTimeout(() => {
+        const loginSuccessful = !!users.find(user => user.username === username && user.password === password)
+        setLoading(false)
+        if (loginSuccessful) {
+          localStorage.setItem("loggedIn", username)
+          setError('Success')
+          return navigate('/')
+        }
+          setError('Wrong username or password')
+      }, 2000)
+    }
+
+
+return (
+    <main className={styles.background} onKeyDown={key => {
+      if (key.key === "Enter")
+        return submit()
+    }}>
+      <section className={styles.container}>
+        <h2 className={styles.title}>Log in</h2>
+        <section className={styles.field}>
+          <label htmlFor="username">
+            E-mail or username
+          </label>
+          <input name="username" onChange={e => setUserName(e.target.value)}/>
+        </section>
+
+        <section className={styles.field}>
+          <label htmlFor="password">
+            Password
+          </label>
+          <input name="password" type="password" onChange={e => setPassword(e.target.value)} />
+        </section>
+
+        <p className={`${styles[error !== "Success" ? 'error' : 'success']} ${error ? styles.show : ''}`}>{error}</p>
+        <div className={styles.button}>
+        <button className={styles.loginButton} onClick={() => submit()}>
+          {loading ? 'Loading...' : 'Log in'}
+        </button>
         </div>
+      </section>
+      <span className={styles.spanclass}>Don't have an account?&nbsp;<a href="http://localhost:8000/signup">Sign up</a></span>
+    </main>
+  )}
 
-        <span className={styles.spanclass}>Don't have an account?&nbsp;<a href="http://localhost:8000/signup">Sign up</a></span>
-    </div>
-)
+
 export default LogInPage
--- src/components/SignUpPage/index.js
+++ src/components/SignUpPage/index.js
@@ -1,38 +1,77 @@
-import React from 'react'
- 
+import React, {useState} from 'react'
+import {navigate} from 'gatsby'
 import styles from './style.module.css';
 
-const SignUpPage = () => (
-    <section className={styles.form}>
-        
-        <div className={styles.signup}>
-            <h2 className={styles.title}>Sign up</h2>
-            <div className={styles.email}>
-                <label htmlFor="email">E-mail:</label>
-                <input name="email" type="text" />
-            </div> 
 
-            <div className={styles.username}>
-                <label htmlFor="username">Username:</label>
-                <input name="username" type="text" />
-            </div>  
+const SignUpPage = () => {
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
+    const [email, setEmail] = useState()
+    const [profession, setProfession] = useState()
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+  
+    const submit = () => {
+      setError(false)
+      setLoading(true)
+      setTimeout(() => {
+        const loginSuccessful = !!(username != '' && password != '')
+        setLoading(false)
+        if (loginSuccessful) {
+          localStorage.setItem("loggedIn", username)
+          setError('Success')
+          return navigate('/')
+        }
+          setError('Wrong input')
+      }, 2000)
+    }
 
-            <div className={styles.password}>
-                <label htmlFor="password">Password:</label>
-                <input name="password" type="text" />
-            </div>
 
-            <div className={styles.profession}>
-                <label htmlFor="profession">Profession:</label>
-                <input name="profession" type="text" />
-            </div> 
+return (
+    <main className={styles.background} onKeyDown={key => {
+      if (key.key === "Enter")
+        return submit()
+    }}>
+      <section className={styles.container}>
+        <h2 className={styles.title}>Sign up</h2>
+        <section className={styles.field}>
+          <label htmlFor="email">
+            E-mail
+          </label>
+          <input name="email" onChange={e => setEmail(e.target.value)}/>
+        </section>
 
-            <div className={styles.buttondiv}>
-                <button className={styles.button}>Sign up</button>
-            </div>
+        <section className={styles.field}>
+          <label htmlFor="username">
+            Username
+          </label>
+          <input name="username" onChange={e => setUserName(e.target.value)}/>
+        </section>
+
+        <section className={styles.field}>
+          <label htmlFor="password">
+            Password
+          </label>
+          <input name="password" type="password" onChange={e => setPassword(e.target.value)} />
+        </section>
+
+        <section className={styles.field}>
+          <label htmlFor="profession">
+            Profession
+          </label>
+          <input name="profession" onChange={e => setProfession(e.target.value)} />
+        </section>
+
+        <p className={`${styles[error !== "Success" ? 'error' : 'success']} ${error ? styles.show : ''}`}>{error}</p>
+        <div className={styles.button}>
+        <button className={styles.loginButton} onClick={() => submit()}>
+          {loading ? 'Loading...' : 'Sign up'}
+        </button>
         </div>
-        
-        <span className={styles.span}>Already have an account?<a href="http://localhost:8000/login">Log in</a></span>
-    </section>
-)
+      </section>
+      <span className={styles.spanclass}>Already have an account?&nbsp;<a href="http://localhost:8000/login">Log in</a></span>
+    </main>
+  )}
+
+
 export default SignUpPage
