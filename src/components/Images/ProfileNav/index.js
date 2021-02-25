97d3d87732ca025b51abd32476fbaa7b4aae4472
petraroso <proso00@fesb.hr>
2021-02-25 14:15:30 +0100

    profile nav

--- src/components/Images/ProfileNav/index.js
+++ src/components/Images/ProfileNav/index.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-import styles from './style.module.css'
-
+//import Img from "gatsby-image"
+import BackgroundImage from 'gatsby-background-image'
+import styles from "./style.module.css"
+ 
 const ProfileNav = () => {
     const data = useStaticQuery(graphql`
     query {
       desktop: file(relativePath: { eq: "profilePicture.jpg" }) {
         childImageSharp {
-          fluid(quality: 90, maxWidth: 85) {
+          fluid(quality: 90, maxWidth: 1920) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
-        extension
-        publicURL
       }
     }
   `)
    return(
-    <div style={{maxWidth: "800px", minWidth: "50px"}}>
-    <Img className = {styles.photo} fluid={data.desktop.childImageSharp.fluid} />
-    </div>)
- }
+    <BackgroundImage
+    Tag = "section"
+    className = {styles.titleImage}
+    fluid={data.desktop.childImageSharp.fluid}
+    backgroundColor={`#040e18`}
+     
+     imgStyle={{ objectFit: "contain",backgroundPositionX:"left"}}
+     style={{ height: "150px", width: "150px", aspectRatio:"160/90" ,borderRadius:"100%"}}
+   >
+   </BackgroundImage>
  
-
+   )
+ }
 export default ProfileNav
--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -5,7 +5,7 @@ import Navigation from '../Navigation'
 import Logo from '../Logo'
 import ProfileNav from '../Images/ProfileNav'
 
-
+import ProfileImage1 from '../Images/ProfileImage1'
 
 
 import styles from './style.module.css'
@@ -22,7 +22,7 @@ const NavigationHeader = ({activeTab}) => (
     <Navigation activeTab = {activeTab} />
     </div>
     <div className = {styles.profileNav}>
-    <ProfileNav  />
+    <ProfileImage1  />
   </div>
   </section>
   </div>
