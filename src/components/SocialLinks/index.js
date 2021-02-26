87cfe69e505aafb931f327c0135b98eb711a586f
{zrinka <zbarac00@fesb.hr>
2021-02-26 11:43:19 +0100

    Editing social links

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './style.module.css'
-import FacebookIcon from '../Images/FacebookIcon'
-import InstagramIcon from '../Images/InstagramIcon'
-//import SnapchatIcon from '../Images/SnapchatIcon'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebook, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons"
+
 
 const SocialLinks = () => (
     <nav className = {styles.SocialLinks}>  
-     <a href="https://hr-hr.facebook.com/"><FacebookIcon/></a> 
-     <a href ="https://www.instagram.com/"><InstagramIcon/></a>
-
+     <a href="https://hr-hr.facebook.com/" className={styles.facebook}><FontAwesomeIcon icon = {faFacebook} size="2x"/></a> 
+     <a href ="https://www.instagram.com/" className={styles.instagram}><FontAwesomeIcon icon = {faInstagram} size="2x"/></a>
+     <a href ="https://twitter.com/login?lang=en" className={styles.twitter}><FontAwesomeIcon icon = {faTwitter} size="2x"/></a>
     </nav>
 )
 export default SocialLinks
