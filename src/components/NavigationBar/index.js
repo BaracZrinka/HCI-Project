3070b0ccc954b35646d829a1511ab8d0d0defe8f
{zrinka <zbarac00@fesb.hr>
2021-03-05 12:26:49 +0100

    Log out implemented

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -3,15 +3,31 @@ import styles from './style.module.css'
 //const navTabs = ['Home', 'Blogs', 'About', 'Log in', 'Your account']
 import { navs as navTabs } from '../../constants/const'
 import { Link } from 'gatsby'
-const NavigationBar = ({activeTab}) => (
+
+const loggedIn = () => !!localStorage.getItem('loggedIn')
+
+
+const NavigationBar = ({activeTab}) => ( <div>
     <nav className={styles.navigationBar}>
+     
       {navTabs.map(({tab, to}) => (
-        <div>
+       
         <Link to={to} >
             <li className={tab === activeTab ? styles.active : ''}>{tab}</li>
         </Link>
-        </div>)
+        )
       )}
-    </nav>
+       <Link to={'/login'}>
+            <li onClick={loggedIn() ? () => localStorage.removeItem('loggedIn') : () => {}}>
+                {loggedIn() ? 'Logout' : 'Login'}
+            </li>
+        </Link>
+        {loggedIn() && <Link to={'/profile'}>
+            <li>
+                Profile
+            </li>
+            </Link>}
+       
+    </nav> </div>  
   )
 export default NavigationBar
--- src/modules/ProfileAll/index.js
+++ src/modules/ProfileAll/index.js
@@ -8,7 +8,7 @@ const ProfileAll = () => {
     const [user, setUser] = useState(localStorage.getItem('loggedIn'))
   
     if (!user) {
-      setTimeout(() => navigate('/login'), 4000)
+      setTimeout(() => navigate('/login'), 2000)
       return <p>
         You cannot view this page without login! <br/>You will be redirected to login page shortly
       </p>}
