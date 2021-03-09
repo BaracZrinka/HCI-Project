cf34fc193fe8aae72ed2a174f963170653477bbf
{zrinka <zbarac00@fesb.hr>
2021-03-09 12:29:57 +0100

    Implementing search bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 import styles from './style.module.css'
 
-const SearchBar = () => {
+/*const SearchBar = () => {
     return(
         <div className={styles.container}>
             <form className={styles.form} action="/blog" method="get">
@@ -13,4 +13,22 @@ const SearchBar = () => {
     )
 }
 
-export default SearchBar
+export default SearchBar*/
+const SearchBar = () => (
+    <>
+    <div className={styles.container}>
+    <form className={styles.form} action="/blog" method="get">
+        
+        <input className={styles.input}
+            type="text"
+            id="header-search"
+            placeholder="Search blog posts"
+            name="s" 
+        />
+        <FaSearch style={{ marginBottom: '-3px' }} className={styles.search}/>
+    </form>
+    </div>
+    </>
+);
+
+export default SearchBar;
