20151b0b913be35d373d557079e143d568e4cfb7
petraroso <proso00@fesb.hr>
2021-03-16 23:22:39 +0100

    Editing css about user on profile

--- src/components/ProfileAbout/index.js
+++ src/components/ProfileAbout/index.js
@@ -1,57 +1,59 @@
-import React,{useState} from 'react'
- 
-import styles from './style.module.css';
-import ProfileNav from '../Images/ProfileNav';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBookmark } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react"
+import styles from "./style.module.css"
+import ProfileNav from "../Images/ProfileNav"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBookmark } from "@fortawesome/free-solid-svg-icons"
 //import {faFileUpload } from '@fortawesome/free-solid-svg-icons'
-import {faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faEdit } from "@fortawesome/free-solid-svg-icons"
 
 /*<i class="far fa-bookmark"></i>*/
 
+const ProfileAbout = ({ name }) => {
+  const [posts, postsCounter] = useState("0")
+  const [followers, followersCounter] = useState("0")
 
-const ProfileAbout = ({name}) => {
-    const [posts, postsCounter]=useState("0");
-    const [followers, followersCounter]=useState("0");
- 
-    return(
+  return (
     <section className={styles.form}>
-            <div className = {styles.firstDiv}>
-                <ProfileNav className={styles.picture}/>
-                    <div className={styles.positioning}>
-                        <>
-                    <div className={styles.counters}>
-                        <h4>{posts}</h4>
-                        <h4>{followers} </h4>
-                    </div>
-                    <div className={styles.description}>
-                        <h4>posts</h4>
-                        <h4>followers</h4>
-                    </div>
-                    </>
-                    </div>
-                </div> 
-               <div className={styles.text}> 
-                <h2 className={styles.username}>{name}</h2>
-           
-                <p>Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in 
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in</p>
-            </div>       
+      <div className={styles.firstDiv}>
+        <div className={styles.pictureDiv}>
+          <ProfileNav />
+        </div>
+        <div className={styles.statistics}>
+          <div className={styles.counters}>
+            <h4>{posts}</h4>
+            <h4>{followers} </h4>
+          </div>
+          <div className={styles.labels}>
+            <h4>posts</h4>
+            <h4>followers</h4>
+          </div>
+        </div>
+      </div>
 
-            <div className={styles.icons}>
-                <div className={styles.link}>
-                    <a href="http://localhost:8000/bookmarks" className = {styles.bookamark}><FontAwesomeIcon icon={faBookmark} size ='2x'  color='#696f45' /></a>  
-                </div>
-                <div className={styles.editPosition}>
-                <FontAwesomeIcon icon={faEdit} size ='2x' color='#696f45'/>
-                </div>
+      <div className={styles.secondDiv}>
+        <div className={styles.heading}>
+          <div className={styles.nameAndEdit}>
+            <h2 className={styles.username}>{name}</h2>
+            <div className={styles.editDiv}>
+              <FontAwesomeIcon icon={faEdit} size="2x" color="gray" />
             </div>
+          </div>
+          <div className={styles.bookmarkDiv}>
+            <a href="http://localhost:8000/bookmarks">
+              <FontAwesomeIcon icon={faBookmark} size="2x" color="gray" />
+            </a>
+          </div>
+        </div>
+        <div className={styles.text}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. 
+          </p>
+        </div>
+      </div>
     </section>
-    
-);
-    };
+  )
+}
 export default ProfileAbout
