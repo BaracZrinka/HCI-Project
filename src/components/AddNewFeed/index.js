5245e24c0a196246de665a52eca12a714737b4b9
{zrinka <zbarac00@fesb.hr>
2021-04-10 20:17:58 +0200

    npm install React Calendar

--- src/components/AddNewFeed/index.js
+++ src/components/AddNewFeed/index.js
@@ -1,115 +1,118 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
 import styles from "./style.module.css"
+import Calendar from "react-calendar"
+import "react-calendar/dist/Calendar.css"
 
-const AddNewFeed = () =>{
-    const [button, setButton] = useState(false)
-    const [count, setCount] = useState(1)
-  const change = () => {
+const AddNewFeed = () => {
+  const [button, setButton] = useState(false)
+  const [count, setCount] = useState(1)
+  const [calendar, setCalendar] = useState("")
+  const [check, setCheck] = useState(false)
+  const change = property => {
     setCount(count + 1)
-    console.log(count)
     setButton(true)
+    if (property) {
+      setCalendar(
+        <Calendar
+          onChange={onDateChange}
+          value={date}
+          showNeighboringMonth={false}
+          locale={"en-US"}
+        />
+      )
+      setCheck(true)
+    }
   }
 
-  let isValid = () =>{
-      console.log("inside the func")
-      console.log("count"+count)
-      if(count === 9){
-          console.log("here")
-        alert('Your post is ready to post! :)')
-      }
-      else{
-          console.log("dont")
-          alert("All fields must be filled!")
-      }
+  const [date, setDate] = useState(new Date())
+
+  const onDateChange = newDate => {
+    setDate(newDate)
+    console.log(newDate)
   }
-  let clearIt = () =>{
-    setButton(false)
-  }
-  const [searchTerm, setTerm] = useState("")
- 
-  const handleChanges = event => {
-    setTerm(event.target.value)
+
+  let isValid = () => {
+    if (count === 9) {
+      alert("Your post is ready to post!  :)")
+    } else {
+      alert("All fields must be filled!")
+    }
   }
   return (
-    <div className = {styles.wholeBlog}>
-    <div className={styles.text}>
-    <textarea
-      onClick={()=>change()}
-      placeholder="Insert title"
-      maxLength="160"
-      value = {button ? searchTerm : ""}
-      onChange = {handleChanges}
-    ></textarea>
-    </div>
-    <div className={styles.pictureDiv}>
+    <div className={styles.wholeBlog}>
+      <div className={styles.add}>Add new post</div>
+      <div className={styles.text}>
+        <textarea
+          onClick={change}
+          placeholder="Insert title"
+          maxLength="160"
+        ></textarea>
+      </div>
+      <div className={styles.pictureDiv}>
         <div>Cover Image</div>
-        <button className={styles.insert} onClick={()=>change()}>Insert file</button>
-    </div>
+        <button className={styles.insert} onClick={change}>
+          Insert file
+        </button>
+      </div>
 
-    <div className={styles.summary}>
-    <textarea
-      onClick={()=>change()}
-      placeholder="Insert summary"
-      maxLength="160"
-      value = {button ? searchTerm : ""}
-      onChange = {handleChanges}
-    ></textarea>
-    </div>
-    <div className={styles.body}>
-    <textarea
-      onClick={()=>change()}
-      placeholder="Insert total caption"
-      maxLength="160"
-      value = {button ? searchTerm : ""}
-      onChange = {handleChanges}
-    ></textarea>
-    </div>
-    <div className={styles.author}>
-    <textarea
-      onClick={()=>change()}
-      placeholder="Insert author's name"
-      maxLength="160"
-      value = {button ? searchTerm : ""}
-      onChange = {handleChanges}
-    ></textarea>
-    </div>
-    <div className={styles.tags}>
-    <textarea
-      onClick={()=>change()}
-      placeholder="Insert post's tags"
-      maxLength="160"
-      value = {button ? searchTerm : ""}
-      onChange = {handleChanges}
-    ></textarea>
-    </div>
-    <div className={styles.slug}>
-    <textarea
-      onClick={()=>change()}
-      placeholder="Insert unique tag for a web path"
-      maxLength="160"
-      value = {button ? searchTerm : ""}
-      onChange = {handleChanges}
-    ></textarea>
-    </div>
-    <div className = {styles.date}>
+      <div className={styles.summary}>
+        <textarea
+          onClick={change}
+          placeholder="Insert summary"
+          maxLength="160"
+        ></textarea>
+      </div>
+      <div className={styles.body}>
+        <textarea
+          onClick={change}
+          placeholder="Insert total caption"
+          maxLength="160"
+        ></textarea>
+      </div>
+      <div className={styles.author}>
+        <textarea
+          onClick={change}
+          placeholder="Insert author's name"
+          maxLength="160"
+        ></textarea>
+      </div>
+      <div className={styles.tags}>
+        <textarea
+          onClick={change}
+          placeholder="Insert post's tags"
+          maxLength="160"
+        ></textarea>
+      </div>
+      <div className={styles.slug}>
+        <textarea
+          onClick={change}
+          placeholder="Insert unique tag for a web path"
+          maxLength="160"
+        ></textarea>
+      </div>
+      <div className={styles.date}>
         <div className={styles.divText}>Choose time of creation</div>
-        <button className={styles.chooseDate} onClick={()=>change()}>Modificate date</button>
+        <button
+          className={styles.chooseDate}
+          onClick={() => change("calendar")}
+        >
+          Modificate date
+        </button>
+      </div>
+      {check ? calendar : ""}
+      {button ? (
+        <div className={styles.commentButtons}>
+          <button className={styles.button1} onClick={() => setButton(false)}>
+            REMOVE
+          </button>
+          <button className={styles.button2} onClick={() => isValid()}>
+            SUBMIT
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-    {button ? (
-            <div className={styles.commentButtons}>
-              <button
-                className={styles.button1}
-                onClick={clearIt}
-              >
-                REMOVE
-              </button>
-              <button className={styles.button2} onClick = {()=>isValid()}>SUBMIT</button>
-            </div>
-          ) : (
-            ""
-    ) }
-
-</div>
   )
 }
 
