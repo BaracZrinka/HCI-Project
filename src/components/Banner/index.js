c32465aa04224fdecc032c5b47de6dea3e1fca16
petraroso <proso00@fesb.hr>
2021-04-13 19:35:36 +0200

    Changing banner picture and letters

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import BackgroundImage from 'gatsby-background-image'
-import styles from './style.module.css'
- 
+import BackgroundImage from "gatsby-background-image"
+import styles from "./style.module.css"
+
 const Banner = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -12,35 +12,30 @@ const Banner = () => {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
-        
       }
     }
-    
   `)
-   return (
-     <div className={styles.banner}>
-     <BackgroundImage
-     Tag = "section"
-     className = {styles.titleImage}
-     fluid={data.desktop.childImageSharp.fluid}
-      backgroundColor={`#040e18`}
-    >
-      
-      <div className = {styles.title}>
-        <h1>BeBIO</h1>
-        <p>
-          Start exploring the world of healthy living
-        </p>
-       </div> 
+  return (
+    <div className={styles.banner}>
+      <BackgroundImage
+        Tag="section"
+        className={styles.titleImage}
+        fluid={data.desktop.childImageSharp.fluid}
+        backgroundColor={`#040e18`}
+      >
+        <div className={styles.title}>
+          <h1>BeBIO</h1>
+          <div className={styles.description}>
+            Start exploring the world of healthy living
+          </div>
+        </div>
         <section className={styles.section}>
           <button className={styles.button1}>Hottest posts</button>
           <button className={styles.button2}>Newest posts</button>
         </section>
-      
-    </BackgroundImage>
+      </BackgroundImage>
     </div>
-
-   )
+  )
 }
- 
+
 export default Banner
