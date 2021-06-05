1d5b180909a3a82fbcc53603c6ca5ba2b261ba60
petraroso <proso00@fesb.hr>
2021-06-05 14:39:32 +0200

    404 page styling

--- src/modules/ProfileAll/index.js
+++ src/modules/ProfileAll/index.js
@@ -3,24 +3,24 @@ import ProfileContainer from "../ProfileContainer"
 import { navigate } from "gatsby"
 import { myLocalStorage } from "../../helper"
 import BlogHeader from "../../components/BlogHeader"
+import styles from "./style.module.css"
 
 const ProfileAll = () => {
   const user = myLocalStorage.getItem("loggedIn")
   const url = typeof window !== "undefined" ? window.location.pathname : ""
- // console.log("path" + url)
+  // console.log("path" + url)
 
   if (url === "/profile") {
     if (!user) {
-      setTimeout(() => navigate("/login"), 2000)
+      setTimeout(() => navigate("/login"), 4000)
       return (
-        <p>
+        <div className={styles.text}>
           You cannot view this page without login! <br />
-          You will be redirected to login page shortly
-        </p>
+          Redirecting to login page...
+        </div>
       )
     }
   }
-
 
   return url === "/profile" ? (
     <ProfileContainer name={user} />
